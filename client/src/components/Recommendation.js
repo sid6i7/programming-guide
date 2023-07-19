bb5a5c242e93635354dbdf5c26615348ff0f5402
Siddhant Jain <siddhantjain209@gmail.com>
2023-07-20 01:15:42 +0530

    add some spacing to components in frontend

--- client/src/components/Recommendation.js
+++ client/src/components/Recommendation.js
@@ -10,9 +10,10 @@ export const Recommendation = ({ stackoverflow, medium, question }) => {
   return (
     <div className='text-center'>
       <h1>Recommendations</h1>
-      <h3>Q) {question}</h3>
+      <h3 className="mb-4">Q) {question}</h3>
+
       <div className="d-flex justify-content-center align-items-center mb-4">
-        <img src={stackoverflowBanner} alt="Stackoverflow Posts" className="img-fluid" style={{ maxHeight: '100px' }} />
+        <img src={stackoverflowBanner} alt="Stackoverflow Posts" className="img-fluid my-4" style={{ maxHeight: '100px' }} />
       </div>
 
       <div className="row row-cols-1 row-cols-md-4 g-4 justify-content-center">
@@ -21,27 +22,27 @@ export const Recommendation = ({ stackoverflow, medium, question }) => {
             <div className="card">
               <div className="card-body">
                 <h5 className="card-title">{question.title}</h5>
-                <p className="card-text">Similarity: {question.similarity}%</p>
-                <a href={`https://stackoverflow.com/questions/${question.id}`} target='_blank' className="btn btn-warning">Read More</a>
+                <p className="card-text">Similarity: {Math.round(question.similarity)}%</p>
+                <a href={`https://stackoverflow.com/questions/${question.id}`} target='_blank' className="btn btn-warning btn-lg">Read More</a> {/* Add btn-lg class to increase button width */}
               </div>
             </div>
           </div>
         ))}
       </div>
 
-      <div className="d-flex justify-content-center align-items-center mb-4"> {/* Add mb-4 for margin-bottom */}
-        <img src={mediumBanner} alt="Medium Articles" className="img-fluid" style={{ maxHeight: '100px' }} />
+      <div className="d-flex justify-content-center align-items-center mb-4">
+        <img src={mediumBanner} alt="Medium Articles" className="img-fluid my-4" style={{ maxHeight: '100px' }} />
       </div>
 
-      <div className="row row-cols-1 row-cols-md-2 g-4 justify-content-center"> {/* Add justify-content-center */}
+      <div className="row row-cols-1 row-cols-md-2 g-4 justify-content-center">
         {medium?.map((article) => (
           <div key={article.title} className="col">
             <div className="card">
               <div className="card-body">
                 <h5 className="card-title">{article.title}</h5>
-                <p className="card-text"><strong>Similarity: </strong>{article.similarity}%</p>
+                <p className="card-text"><strong>Similarity: </strong>{Math.round(article.similarity)}%</p>
                 <p className="card-text"><strong>Tags:</strong> {article.tags.slice(1, -1).replace(/'/g, '')}</p>
-                <a href={article.url} target='_blank' className="btn btn-dark">Read More</a>
+                <a href={article.url} target='_blank' className="btn btn-dark btn-lg">Read More</a> {/* Add btn-lg class to increase button width */}
               </div>
             </div>
           </div>
