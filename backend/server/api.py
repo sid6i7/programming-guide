6f83f5dcd1b98f6468578d35c82521679664104f
Siddhant Jain <siddhantjain209@gmail.com>
2023-07-19 12:49:54 +0530

    in api: add support for cors, change tags type to list

--- backend/server/api.py
+++ backend/server/api.py
@@ -1,18 +1,33 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
+from typing import List
+from fastapi.middleware.cors import CORSMiddleware
 from model.model import RecommendationSystem
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
+)
 system = RecommendationSystem()
 
 class RecommendationRequest(BaseModel):
-    sentence: str
-    tag: str = None
+    question: str
+    tags: List[str] = None
     n: int = 1
+
+    @validator('tags', pre=True)
+    def convert_tags_to_list(cls, value):
+        if isinstance(value, str):
+            return [tag.strip() for tag in value.split(',')]
+        return value
 
 @app.post("/recommend")
 def recommend(request: RecommendationRequest):
-    stack, medium = system.recommend(request.sentence, tag=request.tag, n=request.n)
+    stack, medium = system.recommend(request.question, tags=request.tags, n=request.n)
     return {
         "stackoverflow": stack,
         "medium": medium
