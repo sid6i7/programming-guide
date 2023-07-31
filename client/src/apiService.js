cff2b4145966a57dec5a941812650acbb45a7376
Siddhant Jain <siddhantjain209@gmail.com>
2023-07-31 19:26:12 +0530

    add documentation for api function in react

--- client/src/apiService.js
+++ client/src/apiService.js
@@ -1,3 +1,12 @@
+/**
+ * Submits a form with a question and selected tags to the backend API for recommendations.
+ *
+ * @param {string} question - The user's question to be submitted for recommendations.
+ * @param {Array} selectedTags - An array of selected tags associated with the question.
+ * @param {number} nOfRecommendations - The number of recommendations to be returned.
+ * @returns {Promise<Object>} - A promise that resolves to the response body containing the recommendations.
+ * @throws {Error} - If an error occurs during the API call.
+ */
 const submitForm = async (question, selectedTags, nOfRecommendations) => {
     const recommendEndpoint = process.env.REACT_APP_RECOMMEND_ENDPOINT;
     const apiUrl = process.env.REACT_APP_API_URL;
